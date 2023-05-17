a51ae0ffb50ed83058152aefb1410894e25ad1a4
Xiang-UX <124876636+Xiang-UX@users.noreply.github.com>
2023-05-17 21:22:22 +0800

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -1,60 +1,53 @@
-function sendRequest(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                resolve(xhr.response);
-            } else {
-                reject(xhr.status);
-            }
-        };
-        xhr.onerror = () => {
-            reject('Network error.');
-        };
-        xhr.send();
-    });
+function getIp() {
+    return sendRequest('https://api.ipify.org/')
+        .then((response) => {
+            return response.trim();
+        });
 }
 
-sendRequest('https://api.shserve.cn/api/jinip?type=json')
-    .then((responseText) => {
-        const response = JSON.parse(responseText);
-        const ip = response.ip;
+function getLocation(ip) {
+    return sendRequest(`https://zj.v.api.aa1.cn/api/chinaip/?ip=${ip}`)
+        .then((responseText) => {
+            try {
+                const data = JSON.parse(responseText.replace(/"/g, '\"')).data;
+                console.log(data);
+                // 城市
+                const Country = data.Country;
+                const Province = data.Province;
+                const City= data.City;
+                // 东经西经
+                const Latitude= data.Latitude;
+                const Longitude= data.Longitude;
+                const isp_ip= data.isp;
+                console.log(Longitude);
+                
+                document.getElementById('City').innerText = `${Country} ${Province}省${City}市`;
+                document.getElementById('Latitude').innerText = Latitude;
+                document.getElementById('Longitude').innerText = Longitude;
+                document.getElementById('isp_ip').innerText = isp_ip;
+            
+            } catch (error) {
+                console.error(error);
+            }
+        });
+}
+
+getIp()
+    .then((ip) => {
         console.log(ip);
         document.getElementById('ip').innerText = ip;
-        return sendRequest(`https://zj.v.api.aa1.cn/api/chinaip/?ip=${ip}`);
-    })
-    .then((responseText) => {
-        try {
-            const data = JSON.parse(responseText.replace(/"/g, '\"')).data;
-            console.log(data);
-            // 城市
-            const Country = data.Country;
-            const Province = data.Province;
-            const City= data.City;
-             // 东经西经
-            const Latitude= data.Latitude;
-            const Longitude= data.Longitude;
-            const isp_ip= data.isp;
-            console.log(Longitude);
-            
-            document.getElementById('City').innerText = `${Country} ${Province}省${City}市`;
-            document.getElementById('Latitude').innerText = Latitude;
-            document.getElementById('Longitude').innerText = Longitude;
-            document.getElementById('isp_ip').innerText = isp_ip;
-        
-           
-           
-            // 其他相关数据
-            // const = data. ;
-            // const = data. ;
-            // const = data.
-            // const = data.
-            // const = data.
-        } catch (error) {
-            console.error(error);
-        }
+        return getLocation(ip);
     })
     .catch((error) => {
         console.error(error);
     });
+
+function sendRequest(url) {
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Fetch error: ${response.status}`)
+            }
+            return response.text();
+        });
+}
